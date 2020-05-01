97fd84d155e4dc57857c1587f092a2fec8ad535b
Amber Michelle Webb <amichelle.webb@gmail.com>
2020-04-30 19:16:59 -0500

    Add sample photos

--- src/App/index.js
+++ src/App/index.js
@@ -6,9 +6,9 @@ export default function App() {
   return (
     <div className='App'>
       <Carousel autoScroll autoScrollInterval={5000}>
-        <p>Slide 1</p>
-        <p>Slide 2</p>
-        <p>Slide 3</p>
+        <img src="https://source.unsplash.com/user/erondu/930x600" alt=""/>
+        <img src="https://source.unsplash.com/user/erondu/likes/930x600" alt="" />
+        <img src="https://source.unsplash.com/collection/190727/930x600" alt="" />
       </Carousel>
     </div>
   );
